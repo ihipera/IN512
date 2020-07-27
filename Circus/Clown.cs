3d2ac88508f93aa228ce3a0dbd38a439ce880b63
johnil1 <ihipera1@gmail.com>
2020-07-27 14:49:42 +1200

    Add project files.

---
+++ Circus/Clown.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circus
+{
+    public class Clown
+    {
+        private string name;
+        private int height;
+        private int age;
+
+        public Clown(string name, int height, int age)
+        {
+            this.name = name;
+            this.height = height;
+            this.age = age;
+        }
+
+        public string TalkAboutYourself()
+        {
+            return ("My name is " + name + ". I am " + age + " years old. And I am " + height + " centimeters tall.");
+
+        }
+    }
+}
---
+++ Circus/Form1.cs
@@ -0,0 +1,47 @@
+﻿/* Program name: Circus
+ Project file name: Circus
+ Author: Isobella Johnson 
+ Date: 27/07,20
+ Language: C#
+ Platform: Microsoft Visual Studio 2019
+ Purpose: 
+ Description: Clown program
+ Known Bugs: 
+ Additional Features:
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Circus
+{
+    public partial class Form1 : Form
+    {
+      
+        private Clown [] clowns;
+        public Form1()
+        {
+            InitializeComponent();
+            clowns = new Clown[3];
+            clowns[0] = new Clown("Crusty", 160, 45);
+            clowns[1] = new Clown("Charlie", 100, 18);
+            clowns[2] = new Clown("Clary", 180, 67);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < clowns.Length; i++)
+            {
+                pictureBox1.Image = (Bitmap)Properties.Resources.ResourceManager.GetObject("clown" + (i + 1).ToString());
+                MessageBox.Show(clowns[i].TalkAboutYourself());
+            }       
+        }
+    }
+}
